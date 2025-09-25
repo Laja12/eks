bb30e3e8eab7f5a725c1ee1a862d5f243b32065a
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-25 16:10:45 +0530

    Create main.tf

---
+++ modules/eks/main.tf
@@ -0,0 +1,26 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.node_role_arn
+  version  = var.cluster_version
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+}
+
+resource "aws_eks_node_group" "default" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "default"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
+  scaling_config {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 3
+  }
+  instance_types = ["t3.medium"]
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
