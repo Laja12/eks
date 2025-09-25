51fb6ff3e54259440766b74b7ede8b64313aa829
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-25 16:33:31 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,3 +1,25 @@
+# EKS Cluster Role
+resource "aws_iam_role" "cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Node Role (unchanged)
 resource "aws_iam_role" "node_role" {
   name = "eks-node-role"
 
@@ -13,22 +35,23 @@ resource "aws_iam_role" "node_role" {
   })
 }
 
-# Attach AmazonEKSWorkerNodePolicy
 resource "aws_iam_role_policy_attachment" "worker" {
   role       = aws_iam_role.node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-# Attach AmazonEKS_CNI_Policy
 resource "aws_iam_role_policy_attachment" "cni" {
   role       = aws_iam_role.node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-# Attach ECR Read Only Policy
 resource "aws_iam_role_policy_attachment" "ecr" {
   role       = aws_iam_role.node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+output "cluster_role_arn" {
+  value = aws_iam_role.cluster_role.arn
 }
 
 output "node_role_arn" {
