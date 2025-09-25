5a7c7c109413c2bdc33917781c9d5891b1f8125f
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-25 16:16:20 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "List of CIDR blocks for public subnets"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "List of CIDR blocks for private subnets"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
