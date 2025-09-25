b317604aa4e60d57ec1a36fd7e6f8056ec4b70be
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-25 16:09:44 +0530

    Create main.tf

---
+++ modules/iam/main.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach AmazonEKSWorkerNodePolicy
+resource "aws_iam_role_policy_attachment" "worker" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+# Attach AmazonEKS_CNI_Policy
+resource "aws_iam_role_policy_attachment" "cni" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# Attach ECR Read Only Policy
+resource "aws_iam_role_policy_attachment" "ecr" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+output "node_role_arn" {
+  value = aws_iam_role.node_role.arn
+}
