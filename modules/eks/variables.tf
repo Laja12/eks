a225b425ada60f0d85386961fb55a98708ca91fb
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-25 16:37:44 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,24 +1,4 @@
-variable "cluster_name" {
+variable "cluster_role_arn" {
   type        = string
-  description = "EKS cluster name"
-}
-
-variable "cluster_version" {
-  type        = string
-  description = "EKS Kubernetes version"
-}
-
-variable "subnet_ids" {
-  type        = list(string)
-  description = "Subnets for the EKS cluster and node groups"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID where EKS will be created"
-}
-
-variable "node_role_arn" {
-  type        = string
-  description = "IAM role ARN for the EKS worker nodes"
+  description = "IAM role ARN for EKS cluster"
 }
