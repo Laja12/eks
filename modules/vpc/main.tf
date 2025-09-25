34ca9ebfaee27eb6e7cdad82fb8e23c2f4c7e275
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-25 16:08:36 +0530

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
