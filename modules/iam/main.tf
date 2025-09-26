98ab6436d5d64a8e6d6d74d427143ae1dc200b45
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-26 10:49:06 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -6,7 +6,7 @@ resource "aws_iam_role" "cluster_role" {
     Version = "2012-10-17",
     Statement = [{
       Effect = "Allow",
-      Principal = { Service = "eks.amazonaws.com" },
+      Principal = { Service = "ec2.amazonaws.com" },
       Action = "sts:AssumeRole"
     }]
   })
