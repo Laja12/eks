3e67b6bcd47586d1b7e6e22a2f65d97382d3659b
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-26 11:37:31 +0530

    Create k8s.tf

---
+++ k8s.tf
@@ -0,0 +1,21 @@
+# Ensure Kubernetes provider is configured after EKS is ready
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+# Apply your deployment.yaml
+resource "kubernetes_manifest" "my_app" {
+  manifest = yamldecode(file("${path.module}/k8s/deployment.yaml"))
+}
+
+
