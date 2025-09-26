d4c0672b5e5c113594cf76bea5667744e402546d
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-26 10:25:48 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,14 +1,12 @@
-# EKS Cluster Role
+# Cluster Role
 resource "aws_iam_role" "cluster_role" {
   name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
+      Effect = "Allow",
+      Principal = { Service = "eks.amazonaws.com" },
       Action = "sts:AssumeRole"
     }]
   })
@@ -19,34 +17,32 @@ resource "aws_iam_role_policy_attachment" "cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# Node Role (unchanged)
-resource "aws_iam_role" "node_role" {
-  name = "eks-node-role"
+# Nodegroup Role
+resource "aws_iam_role" "nodegroup_role" {
+  name = "eks-nodegroup-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "eks.amazonaws.com" },
       Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
     }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "worker" {
-  role       = aws_iam_role.node_role.name
+  role       = aws_iam_role.nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "cni" {
-  role       = aws_iam_role.node_role.name
+  role       = aws_iam_role.nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "ecr" {
-  role       = aws_iam_role.node_role.name
+  role       = aws_iam_role.nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
@@ -54,6 +50,6 @@ output "cluster_role_arn" {
   value = aws_iam_role.cluster_role.arn
 }
 
-output "node_role_arn" {
-  value = aws_iam_role.node_role.arn
+output "nodegroup_role_arn" {
+  value = aws_iam_role.nodegroup_role.arn
 }
