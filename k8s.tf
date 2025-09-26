2e506ba58743445eabd0d2128f4c58fdd220b90b
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-26 13:04:34 +0530

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -6,6 +6,14 @@ resource "null_resource" "wait_for_eks" {
   }
 }
 
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
