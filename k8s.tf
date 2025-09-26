a53a5bdc3849d50e3a96c1f464d066db16d41a95
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-26 12:48:13 +0530

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -8,12 +8,13 @@ data "aws_eks_cluster_auth" "this" {
   depends_on = [module.eks]
 }
 
+
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
-  
 }
+
 
 # Split YAMLs into separate files
 resource "kubernetes_manifest" "my_app" {
