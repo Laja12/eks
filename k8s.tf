773771f76ed00f8ab951f0fea13026db545048d5
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-26 12:58:46 +0530

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -1,13 +1,10 @@
-data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
-  depends_on = [module.eks]  # ensure EKS cluster is created first
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
+resource "null_resource" "wait_for_eks" {
   depends_on = [module.eks]
-}
 
+  provisioner "local-exec" {
+    command = "echo EKS cluster is ready"
+  }
+}
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
@@ -15,15 +12,12 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
-
-# Split YAMLs into separate files
 resource "kubernetes_manifest" "my_app" {
-  manifest = yamldecode(file("${path.module}/k8s/deployment.yaml"))
+  manifest  = yamldecode(file("${path.module}/k8s/deployment.yaml"))
+  depends_on = [null_resource.wait_for_eks]
 }
 
-# Apply your service.yaml
 resource "kubernetes_manifest" "my_service" {
-  manifest = yamldecode(file("${path.module}/k8s/service.yaml"))
+  manifest  = yamldecode(file("${path.module}/k8s/service.yaml"))
+  depends_on = [null_resource.wait_for_eks]
 }
-
-
