21044aaa5432f29c73a96ff41907aeeb87f67681
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-26 12:42:35 +0530

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -15,6 +15,11 @@ provider "kubernetes" {
   
 }
 
+# Split YAMLs into separate files
+resource "kubernetes_manifest" "my_app" {
+  manifest = yamldecode(file("${path.module}/k8s/deployment.yaml"))
+}
+
 # Apply your service.yaml
 resource "kubernetes_manifest" "my_service" {
   manifest = yamldecode(file("${path.module}/k8s/service.yaml"))
