c4d888c034ff3a9a26ef907d9bf4d17fef6b147b
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-26 11:14:39 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,4 +1,6 @@
-# Cluster Role
+############################################
+# EKS Cluster Role
+############################################
 resource "aws_iam_role" "cluster_role" {
   name = "eks-cluster-role"
 
@@ -6,18 +8,28 @@ resource "aws_iam_role" "cluster_role" {
     Version = "2012-10-17",
     Statement = [{
       Effect = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" },
+      Principal = {
+        Service = "eks.amazonaws.com"
+      },
       Action = "sts:AssumeRole"
     }]
   })
 }
 
+# Attach policies for cluster
 resource "aws_iam_role_policy_attachment" "cluster_policy" {
   role       = aws_iam_role.cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# Nodegroup Role
+resource "aws_iam_role_policy_attachment" "cluster_vpc" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+############################################
+# EKS Node Group Role (EC2 based nodes)
+############################################
 resource "aws_iam_role" "nodegroup_role" {
   name = "eks-nodegroup-role"
 
@@ -25,12 +37,15 @@ resource "aws_iam_role" "nodegroup_role" {
     Version = "2012-10-17",
     Statement = [{
       Effect = "Allow",
-      Principal = { Service = "eks.amazonaws.com" },
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
       Action = "sts:AssumeRole"
     }]
   })
 }
 
+# Attach required policies to node group
 resource "aws_iam_role_policy_attachment" "worker" {
   role       = aws_iam_role.nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
@@ -46,10 +61,15 @@ resource "aws_iam_role_policy_attachment" "ecr" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+############################################
+# Outputs
+############################################
 output "cluster_role_arn" {
-  value = aws_iam_role.cluster_role.arn
+  description = "IAM Role ARN for EKS cluster"
+  value       = aws_iam_role.cluster_role.arn
 }
 
 output "nodegroup_role_arn" {
-  value = aws_iam_role.nodegroup_role.arn
+  description = "IAM Role ARN for EKS managed node group"
+  value       = aws_iam_role.nodegroup_role.arn
 }
