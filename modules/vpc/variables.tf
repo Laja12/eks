d4eff695c43087f651084b8995d7a1f0d8a41bc9
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-09-26 10:22:23 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,19 +1,4 @@
-variable "vpc_cidr" {
-  type        = string
-  description = "CIDR block for the VPC"
-}
-
-variable "public_subnets" {
-  type        = list(string)
-  description = "List of CIDR blocks for public subnets"
-}
-
-variable "private_subnets" {
-  type        = list(string)
-  description = "List of CIDR blocks for private subnets"
-}
-
-variable "availability_zones" {
-  type        = list(string)
-  description = "List of availability zones"
-}
+variable "vpc_cidr" { type = string }
+variable "public_subnets" { type = list(string) }
+variable "private_subnets" { type = list(string) }
+variable "availability_zones" { type = list(string) }
